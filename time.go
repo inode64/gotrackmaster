bfbbde2f17828e0b973d92359e6af436e7f86b64
Fco Javier Felix <ffelix@inode64.com>
2023-05-13 09:30:22 +0200

    Include support to fix times

--- time.go
+++ time.go
@@ -21,3 +21,47 @@ func TimeDiff(w, pt gpx.WptType) float64 {
 	}
 	return delta.Seconds()
 }
+
+// FixTimesSegment fixes the time of a track segment.
+func FixTimesSegment(tr gpx.TrkSegType) gpx.TrkSegType {
+	if len(tr.TrkPt) == 0 {
+		return tr
+	}
+
+	// Check first element
+	if !tr.TrkPt[0].Time.IsZero() && tr.TrkPt[0].Time.After(tr.TrkPt[1].Time) {
+		tr.TrkPt[0].Time = tr.TrkPt[1].Time.Add(-10 * time.Second)
+	}
+
+	// Check all intermediate elements
+	lastValidTime := tr.TrkPt[0].Time
+	totalDiff := time.Duration(0)
+	for i := 1; i < len(tr.TrkPt)-1; i++ {
+		if tr.TrkPt[i].Time.IsZero() {
+			continue
+		}
+		if tr.TrkPt[i].Time.After(tr.TrkPt[i+1].Time) || tr.TrkPt[i].Time.Before(lastValidTime) {
+			averageDiff := totalDiff / time.Duration(i)
+			tr.TrkPt[i].Time = lastValidTime.Add(averageDiff)
+		} else {
+			totalDiff += tr.TrkPt[i].Time.Sub(lastValidTime)
+			lastValidTime = tr.TrkPt[i].Time
+		}
+	}
+
+	// Check last element
+	if tr.TrkPt[len(tr.TrkPt)-1].Time.Before(lastValidTime) {
+		tr.TrkPt[len(tr.TrkPt)-1].Time = lastValidTime.Add(totalDiff / time.Duration(len(tr.TrkPt)-1))
+	}
+
+	return tr
+}
+
+// FixTimesTrack fixes the time of a track.
+func FixTimesTrack(g gpx.GPX) {
+	for TrkTypeNo, TrkType := range g.Trk {
+		for TrkSegTypeNo, TrkSegType := range TrkType.TrkSeg {
+			*g.Trk[TrkTypeNo].TrkSeg[TrkSegTypeNo] = FixTimesSegment(*TrkSegType)
+		}
+	}
+}
