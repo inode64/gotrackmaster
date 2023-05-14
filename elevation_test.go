18489752a0d89b86b0c4367bdc503d8f63ea0649
Fco Javier Felix <ffelix@inode64.com>
2023-05-14 20:32:27 +0200

    Lost elevation test

---
+++ elevation_test.go
@@ -0,0 +1,26 @@
+package trackmaster_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	gpx "github.com/twpayne/go-gpx"
+
+	trackmaster "github.com/inode64/gotrackmaster"
+)
+
+// testSpeedFix tests the speed fix algorithm.
+func TestLostElevation(t *testing.T) {
+	filename := "testdata/2020-12-19_11-14_Sat_benitandus.gpx"
+	t.Run(filename, func(t *testing.T) {
+		f, err := os.Open(filename)
+		assert.NoError(t, err)
+		defer f.Close()
+		g, err := gpx.Read(f)
+		assert.NoError(t, err)
+		assert.NotNil(t, g)
+		trackmaster.LostElevation(*g, true)
+		assert.Equal(t, g.Trk[0].TrkSeg[0].TrkPt[3655].Ele, 468.95)
+	})
+}
