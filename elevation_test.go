0c10a56421ae7e6d347b62fdd6f657da26f05227
Fco Javier Felix <ffelix@inode64.com>
2023-05-17 18:01:33 +0200

    New test for ElevationSRTM(

--- elevation_test.go
+++ elevation_test.go
@@ -23,3 +23,18 @@ func TestLostElevation(t *testing.T) {
 		assert.Equal(t, g.Trk[0].TrkSeg[0].TrkPt[3655].Ele, 468.95)
 	})
 }
+
+func TestLostElevationSRTM(t *testing.T) {
+	filename := "testdata/2023-03-05_09-27_Sun.gpx"
+	t.Run(filename, func(t *testing.T) {
+		f, err := os.Open(filename)
+		assert.NoError(t, err)
+		defer f.Close()
+		g, err := gpx.Read(f)
+		assert.NoError(t, err)
+		assert.NotNil(t, g)
+		trackmaster.ElevationSRTM(*g, 5,true)
+		assert.Equal(t, g.Trk[0].TrkSeg[2].TrkPt[265].Ele, 721.0)
+		assert.Equal(t, g.Trk[0].TrkSeg[2].TrkPt[601].Ele, 852.0)
+	})
+}
